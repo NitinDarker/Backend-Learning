b138c76c54f678d902613dcb55822d9e843d7db3
Nitin Sharma <165761502+NitinDarker@users.noreply.github.com>
2025-01-07 20:25:16 +0530

    Add files via upload

---
+++ 02. Express/typeof.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+console.log( "\n\n",
+    "typeof express: ", typeof express , "\n\n",
+    "typeof app: ", typeof app, "\n\n",
+    "express: \n", express, "\n\n\n",
+    "app: \n", app
+);
