64da673dfc85500c92104101f180ea099d8ff947
Nitin Sharma <nitinsharma9829547681@gmail.com>
2025-02-03 22:50:14 +0530

    Trying useEffect hook for the first time

--- 9_React_Foundation/react-hooks/src/App.jsx
+++ 9_React_Foundation/react-hooks/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [person, addPerson] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function updatePerson() {
+        console.log("hi")
+    }
+    useEffect(() => {
+        fetch("https://fakerapi.it/api/v1/persons")
+            .then (async function (res) {
+                const json = await res.json();
+                addPerson(json.data)
+            })
+    }, [])
+
+    return (
+        <>
+        <button onClick={updatePerson}>Add person</button>
+        <div>
+            {person.map((p) => <PersonCont key={p.id} firstname={p.firstname} email={p.email}></PersonCont>)}
+        </div>
+        </>
+    )
+    
 }
 
-export default App
+function PersonCont(props) {
+    return (<>
+        <div>{props.firstname}</div>
+        <div>{props.email}</div>
+    </>
+    )
+}
+
+export default App;
